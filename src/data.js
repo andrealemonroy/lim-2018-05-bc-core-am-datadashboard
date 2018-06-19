bf38c374e01fcc484c75140f6e6e5d41219dabc1
HeydyCH <heidich8@gmail.com>
2018-06-19 12:55:32 -0500

    Creacion de funcion computeUserStast(user,progress,courses)

--- src/data.js
+++ src/data.js
@@ -1,5 +1,19 @@
 window.computeUsersStats = (users, progress, courses) => {
-
+    dataUsers=users;
+    dataProgress=progress;
+    let resultprueba2 = document.getElementById('prueba2'); 
+    for (var i in dataUsers) {
+        for (var j in dataProgress) {
+            if(dataUsers[i].id === j ){
+                var course = dataProgress[j];
+                for (var k in course) {
+                resultprueba2.innerHTML +=  dataUsers[i].name + "  su percent es :  " + course[k].percent + "<hr/>";
+                }
+            }   
+        }    
+    }
+    resu= " // " + courses ;
+    return resu ;
 }
 
 window.sortUsers = (users, orderBy, orderDirection) => {
